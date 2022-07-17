d0b0557f5ba04641316db57614074659497aae0e
dhanielfaraby <58141800+dhanielfaraby@users.noreply.github.com>
2022-07-18 02:42:21 +0700

    Update and rename Skripsi_Coba_Layar_LCD.ino to SkripsiFinal.ino

--- SkripsiFinal.ino
+++ SkripsiFinal.ino
@@ -1,5 +1,4 @@
 #include <LiquidCrystal_I2C.h>
-
 #include <Wire.h> 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
  const int Relay1=53;
@@ -26,7 +25,6 @@ void loop() {
   int nilaisensorair = analogRead(A0);
   int nilaisensortanah = analogRead(A1);
   int nilaisensorldr = analogRead(A2); 
-  
 
     if (nilaisensorair > 550) {
       Serial.println("===============");
@@ -39,32 +37,6 @@ void loop() {
         lcd.setCursor(0,1);
         lcd.print("Kapasitas air : ");
         lcd.print(nilaisensorair);
-      Serial.print("Level Kelembapan Tanah : ");
-        if (nilaisensortanah > 900){
-          Serial.println("Kering ");
-          lcd.setCursor(0,2);
-          lcd.print("Kelembapan : ");
-          lcd.print("Kering");
-        }
-        else{
-          Serial.println("Basah ");
-          lcd.setCursor(0,2);
-          lcd.print("Kelembapan : ");
-          lcd.print("Basah");
-        }
-      Serial.print("Keadaan Cahaya : ");
-      if (nilaisensorldr < 580){
-        Serial.println("Cahaya Terang, Lampu Mati ");
-        lcd.setCursor(0,3);
-        lcd.print("Lampu Mati");
-        digitalWrite(Lampu, LOW);
-      }
-      else{
-        Serial.println("Cahaya Gelap, Lampu Hidup ");
-        lcd.setCursor(0,3);
-        lcd.print("Lampu Hidup");
-        digitalWrite(Lampu, HIGH);
-      }
       digitalWrite(Relay1, HIGH); //Relay High , Rangkaian terbuka, Pompa Mati
       Serial.println("===============");
       delay (2000);
@@ -81,32 +53,6 @@ void loop() {
         lcd.setCursor(0,1);
         lcd.print("Kapasitas air : ");
         lcd.print(nilaisensorair);
-      Serial.print("Level Kelembapan Tanah : ");
-       if (nilaisensortanah > 900){
-          Serial.println("Kering ");
-          lcd.setCursor(0,2);
-          lcd.print("Kelembapan : ");
-          lcd.print("Kering");
-        }
-        else{
-          Serial.println("Basah ");
-          lcd.setCursor(0,2);
-          lcd.print("Kelembapan : ");
-          lcd.print("Basah");
-        }
-        Serial.print("Keadaan Cahaya : ");
-      if (nilaisensorldr < 500){
-        Serial.println("Cahaya Terang, Lampu Mati ");
-        lcd.setCursor(0,3);
-        lcd.print("Lampu Mati");
-        digitalWrite(Lampu, LOW);
-      }
-      else{
-        Serial.println("Cahaya Gelap, Lampu Hidup ");
-        lcd.setCursor(0,3);
-        lcd.print("Lampu Hidup");
-        digitalWrite(Lampu, HIGH);
-      }
       digitalWrite(Relay1, LOW); 
       Serial.println("===============");
 
